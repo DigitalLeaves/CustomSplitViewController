250b9f1f0974057c76b6fb7adb8e95fd79e05a45
Ignacio Nieto <contact@digitalleaves.com>
2017-01-03 13:11:18 +0100

    Initial Commit

---
+++ CustomSplitControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSplitControl
+//
+//  Created by Ignacio Nieto Carvajal on 3/1/17.
+//  Copyright © 2017 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
